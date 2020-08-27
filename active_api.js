7ea2c137031aa45987db76133a1786b92399916a
trilambda122 <trilambda122@gmail.com>
2020-08-27 13:37:55 -0500

    Update active_api.js

--- active_api.js
+++ active_api.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
     var ACTIVE_API_KEY = 'fza3kapxc7dgqnj28uad8v4x';
     var activeDate = "2020-08-26";
-    var activeURL = `http://api.amp.active.com/v2/search?query=category=event&start_date=${activeDate}..&near=Austin,TX,US&radius=100&api_key=${ACTIVE_API_KEY}`;
+    var activeURL = `https://api.amp.active.com/v2/search?query=category=event&start_date=${activeDate}..&near=Austin,TX,US&radius=100&api_key=${ACTIVE_API_KEY}`;
 
 
     var CAMPSITE_KEY = 'p4yw44kxbzz25cehg6e8n7da';
